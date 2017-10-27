518dd6c3f754766caa648080c71d791c2853b524
Alunos <bernardo.carvalho@tecnico.ulisboa.pt>
2017-10-27 15:29:22 +0100

    Added arduino-mk files

---
+++ env-vars.sh
@@ -0,0 +1,7 @@
+# for arduino-mk on Debian Linux
+# apt-get install arduino-mk
+#
+export ARDUINO_DIR=/usr/share/arduino
+export ARDMK_DIR=/usr/share/arduino
+#export ARDMK_DIR=/usr
+export AVR_TOOLS_DIR=/usr
